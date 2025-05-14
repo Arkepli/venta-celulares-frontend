99dc9fac77a7797e6d120bcc180a546d13d9af21
karol0910 <karitoko2020@gmail.com>
2025-05-14 17:29:24 -0500

    Proveedores

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './views/Home';
-/*import Login from './views/Login';
-import Register from './views/Register';
-import Cart from './views/Cart';*/
-import AdminDashboard from './views/AdminDashboard/AdminDashboard';
-import Tecnico from './views/tecnico/Tecnico'
-import Cart from './views/cart/Cart';
+import Navbar from './components/Navbar';
+import AdminDashboard from './pages/AdminDashboard';
+import Home from './pages/Home';
+import Tecnico from './pages/Tecnico';
+import Cart from './pages/Cart';
+import Provider from './views/provider/Provider';
 
 function App() {
   return (
-    <Routes>
+    <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/adminDashboard" element={<AdminDashboard />} />
         <Route path="/tecnico" element={<Tecnico />} />
-        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/cart" element={<Cart />} />
+        <Route path='/provider' element= {<Provider/>} />
       </Routes>
-    </Routes>
+    </Router>
   );
 }
 
