c7277565655aeb0503ff06114b0c0fb8bf7b63e0
Alexander1096 <cyasno31@gmail.com>
2025-05-14 22:04:56 -0500

    Nuevas funcionalidades

--- src/views/vendedor/Sale.jsx
+++ src/views/vendedor/Sale.jsx
@@ -84,13 +84,6 @@ const Sale = () => {
     <div className="tecnico-container">
       <h2 className="titulo">Ventas</h2>
 
-      {/* <div className="tabs">
-        <span className="tab activo">Citas</span>
-        <span className="tab">Reparados</span>
-        <span className="tab">Garantía</span>
-        <span className="tab">Reclamos&Felicitaciones</span>
-      </div> */}
-
       <div className="filtros">
         <div class="saleFiltros">
           <div class="saleWrapper">
@@ -116,7 +109,7 @@ const Sale = () => {
             Limpiar
           </button>
           <button className="btn consultar" onClick={handleConsultar}>
-            Consultar
+            Agregar
           </button>
         </div>
       </div>
@@ -142,7 +135,26 @@ const Sale = () => {
               <td>{item.precio}</td>
               <td>{item.cantidad}</td>
               <td>{item.fecha}</td>
-              <td>{item.accion}</td>
+              <td>
+                <button
+                  title="Editar"
+                  style={{
+                    background: "none",
+                    border: "none",
+                    cursor: "pointer",
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    width="20"
+                    fill="#000000"
+                  >
+                    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.83z" />
+                  </svg>
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
