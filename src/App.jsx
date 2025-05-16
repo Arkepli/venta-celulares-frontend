bfba90efcc2cdbdca9a2f1bc8f58816ea362250e
David Santiago Bonilla Achury <david_bonillaac@fet.edu.co>
2025-05-15 19:40:33 -0500

    Conflicto con los estilos resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
   import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
   import Navbar from "./components/Navbar";
-  import AdminDashboard from "./views/AdminDashboard/AdminDashboard";
   import Home from "./views/Home";
-  import Tecnico from "./views/tecnico/Tecnico";
-  import Cart from "./views/cart/Cart";
   import Provider from "./views/provider/Provider";
   import Brands from "./views/brands/brands";
   import Celulares from "./views/products/Celulares";
@@ -16,9 +13,6 @@
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/adminDashboard" element={<AdminDashboard />} />
-          <Route path="/tecnico" element={<Tecnico />} />
-          <Route path="/cart" element={<Cart />} />
           <Route path="/provider" element={<Provider />} />
           <Route path="/brands" element={<Brands />} />
           <Route path="/celulares" element={<Celulares />} />
